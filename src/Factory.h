69f0e8becabbf05b22be88411ba9abc8f900de40
Matthew Ballance <matt.ballance@gmail.com>
2022-12-23 14:56:42 -0800

    First pass at separating debug mgr from other code
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ src/Factory.h
@@ -0,0 +1,47 @@
+/**
+ * Factory.h
+ *
+ * Copyright 2022 Matthew Ballance and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ * Created on:
+ *     Author: 
+ */
+#pragma once
+#include <memory>
+#include "dmgr/IFactory.h"
+#include "DebugMgr.h"
+
+namespace dmgr {
+
+class Factory;
+using FactoryUP=std::unique_ptr<Factory>;
+class Factory : public virtual IFactory {
+public:
+    Factory();
+
+    virtual ~Factory();
+
+    virtual IDebugMgr *getDebugMgr() override;
+
+    static Factory *inst();
+
+private:
+    DebugMgrUP                  m_dbg_mgr;
+    static FactoryUP            m_inst;
+};
+
+}
+
+

fc13a0f29fd185cb68b70394a372b7a3f41fcf35
Matthew Ballance <matt.ballance@gmail.com>
2023-08-15 18:25:52 -0700

    Experimenting with CI
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- setup.py
+++ setup.py
@@ -2,15 +2,10 @@
 #* setup.py for debug-mgr
 #****************************************************************************
 import os
-import shutil
 import subprocess
 import sys
-import sysconfig
 from setuptools import setup
 from distutils.extension import Extension
-from distutils.command.build_clib import build_clib
-from distutils.ccompiler import new_compiler
-from distutils.spawn import find_executable
 from setuptools.command.build_ext import build_ext as _build_ext
 from distutils.file_util import copy_file
 
@@ -125,9 +120,7 @@ else:
 if result.returncode != 0:
     raise Exception("build failed")
 
-extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
 extra_compile_args = []
-#extra_compile_args += ["-std=c++11", "-Wall", "-Wextra"]
 if _DEBUG:
     extra_compile_args += ["-g", "-O0", "-DDEBUG=%s" % _DEBUG_LEVEL, "-UNDEBUG"]
 else:
@@ -136,76 +129,13 @@ else:
 if sys.platform == "darwin":
     extra_compile_args += ["-std=c++11"]
 
-def find_source(bases):
-    ret = []
-    for base in bases:
-        for file in os.listdir(base):
-            if os.path.splitext(file)[1] in (".cpp", ".c"):
-                ret.append(os.path.join(base, file))
-            
-    print("find_source: " + str(ret))
-    return ret
-
-def _get_lib_ext_name():
-    """ Get name of default library file extension on given OS. """
-
-    if os.name == "nt":
-        ext_name = "dll"
-    else:
-        ext_name = "so"
-
-    return ext_name
-
 class build_ext(_build_ext):
     def run(self):
-
-        # TODO:
-#        def_dir = os.path.join(cocotb_share_dir, "def")
-#        self._gen_import_libs(def_dir)
-
         super().run()
 
     # Needed for Windows to not assume python module (generate interface in def file)
     def get_export_symbols(self, ext):
         return None
-
-    # For proper cocotb library naming, based on https://github.com/cython/cython/issues/1740
-#    def get_ext_filename(self, ext_name):
-#        """
-#        Like the base class method, but for libraries that are not python extension:
-#         - removes the ``.cpython-36m-x86_64-linux-gnu.`` part before the extension
-#         - replaces ``.pyd`` with ``.dll`` on windows.
-#        """
-#
-#        filename = _build_ext.get_ext_filename(self, ext_name)
-#
-#        # for the simulator python extension library, leaving suffix in place
-#        # TODO:
-##        if "simulator" == os.path.split(ext_name)[-1]:
-##            return filename
-#
-#        head, tail = os.path.split(filename)
-#        tail_split = tail.split(".")
-#
-#        filename_short = os.path.join(head, tail_split[0] + "." + _get_lib_ext_name())
-#
-#        # icarus requires vpl extension, gpivpi is default in Makefiles
-#        # TODO:
-##        if "icarus" in filename:
-##            filename_short = filename_short.replace("libvpi.so", "gpivpi.vpl")
-##            filename_short = filename_short.replace("libvpi.dll", "gpivpi.vpl")
-#
-#        return filename_short
-
-#    def finalize_options(self):
-#        """ Like the base class method,but add extra library_dirs path. """
-#
-#        super().finalize_options()
-#
-#        for ext in self.extensions:
-#            ext.library_dirs.append(
-#                os.path.join(self.build_lib, os.path.dirname(ext._full_name))
-#            )
 
     def copy_extensions_to_source(self):
         """ Like the base class method, but copy libs into proper directory in develop. """
@@ -235,59 +165,20 @@ class build_ext(_build_ext):
         dest_filename = os.path.join(package_dir, filename)
         
         print("package_dir: %s dest_filename: %s" % (package_dir, dest_filename))
-        
-        return
-
-        build_py = self.get_finalized_command("build_py")
-        for ext in self.extensions:
-            fullname = self.get_ext_fullname(ext.name)
-            filename = self.get_ext_filename(fullname)
-            modpath = fullname.split(".")
-            package = ".".join(modpath[:-1])
-            package_dir = build_py.get_package_dir(package)
-            # unlike the method from `setuptools`, we do not call `os.path.basename` here
-            dest_filename = os.path.join(package_dir, filename)
-            src_filename = os.path.join(self.build_lib, filename)
-
-            os.makedirs(os.path.dirname(dest_filename), exist_ok=True)
-
-            copy_file(
-                src_filename, dest_filename, verbose=self.verbose, dry_run=self.dry_run
-            )
-            if ext._needs_stub:
-                self.write_stub(package_dir or os.curdir, ext, True)
-
-    def _gen_import_libs(self, def_dir):
-        """
-        On Windows generate import libraries that contains the code required to
-        load the DLL (.a) based on module definition files (.def)
-        """
-
-        # TODO:
-        if os.name == "nt":
-            for sim in ["icarus", "modelsim", "aldec"]:
-                subprocess.run(
-                    [
-                        "dlltool",
-                        "-d",
-                        os.path.join(def_dir, sim + ".def"),
-                        "-l",
-                        os.path.join(def_dir, "lib" + sim + ".a"),
-                    ]
-                )
 
 print("extra_compile_args=" + str(extra_compile_args))
+
+if sys.platform == "darwin":
+    libext = ".dylib"
+else:
+    libext = ".so"
+
+libpref = "lib"
 
 ext = Extension("debug_mgr.core",
             extra_compile_args=extra_compile_args,
             sources=[
                 os.path.join(debug_mgr_dir, 'python', "core.pyx"), 
-#                os.path.join(libvsc_dir, 'python', 'VisitorProxy.cpp'),
-#                os.path.join(libvsc_dir, 'python', 'py_get_vsc.cpp'),
-#                os.path.join(libvsc_dir, 'python', 'ModelFieldDataClosure.cpp'),
-#                os.path.join(libvsc_dir, 'python', 'ModelStructCreateHookClosure.cpp'),
-#                os.path.join(libvsc_dir, 'python', 'MkModelBuildContext.cpp'),
-#                os.path.join(libvsc_dir, 'python', 'VscTasks.cpp'),
             ],
             language="c++",
             include_dirs=[
@@ -302,6 +193,7 @@ setup(
   version=version,
   packages=['debug_mgr'],
   package_dir = {'' : 'python'},
+  package_data={ 'debug_mgr': ["%sdebug-mgr%s" % (libpref, libext)] },
   author = "Matthew Ballance",
   author_email = "matt.ballance@gmail.com",
   description = ("Simple debug manager for use of C++ Python extensions"),

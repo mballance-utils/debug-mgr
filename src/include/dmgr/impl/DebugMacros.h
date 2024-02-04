53274ca2739dfb628bdb89f74deeedb15ec2c108
Matthew Ballance <matt.ballance@gmail.com>
2024-02-04 13:52:57 -0800

    Add DEBUG_EN macro
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/include/dmgr/impl/DebugMacros.h
+++ src/include/dmgr/impl/DebugMacros.h
@@ -8,6 +8,7 @@
     if (!m_dbg) { \
         m_dbg = ((mgr))?(mgr)->findDebug(scope):0; \
     }
+#define DEBUG_EN (m_dbg && m_dbg->en())
 #define DEBUG_ENTER(fmt, ...) \
     if (m_dbg && m_dbg->en()) m_dbg->enter(fmt, ##__VA_ARGS__)
 #define DEBUG_LEAVE(fmt, ...) \
